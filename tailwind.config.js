a23a098358494f0c2989c04d7db105172affba04
Jimoh Olamide <jolamideprince@gmail.com>
2024-05-27 18:58:55 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      screens: {
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1536px',
+      },
+
+      colors: {
+        mblue: 'rgba(35, 74, 214, 1)',
+        customgrey: 'rgba(244, 245, 247, 1)',
+        lpurple: 'rgba(213, 219, 242, 1)',
+      },
+
+      backgroundImage: {
+         'custom-img': "url('./images/bg-img.png')",
+      },
+     
+    },
+  },
+  plugins: [],
+}
+
